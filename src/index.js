fd531c0c8e6f458614b6090355fbb542b7d9b696
César Grisel <griselcesar@gmail.com>
2023-10-05 22:50:21 -0400

    Agregamos dotenv y gestionamos conexión de db

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Hola node")
+import mongoose from 'mongoose'
+import { config } from 'dotenv'
+import app from './app.js'
+
+config()
+
+
+mongoose.connect(process.env.URI_DB)
+.then(
+  console.log('📦 db connect successful'),
+  app.listen(process.env.PORT, () => {
+    console.log(`🚀 api run on port ${process.env.PORT}`)
+  })
+).catch(err => {
+  console.log(err)
+})
